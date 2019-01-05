5e772ce002f409c145b124c25fec6009558b8f1f
juju4 <juju4@users.noreply.github.com>
2019-01-05 17:36:06 +0000

    squid openssl from deb source: update serverspec tests

--- test/integration/default-https/serverspec/https_spec.rb
+++ test/integration/default-https/serverspec/https_spec.rb
@@ -5,14 +5,30 @@ set :backend, :exec
 
 proxy_port = 3128
 
-describe command("curl -v -x http://localhost:#{proxy_port} https://www.google.com") do
+describe command("curl -v -x http://localhost:#{proxy_port} https://www.google.com"), :if => os[:family] == 'redhat' do
   its(:stdout) { should match /<title>Google<\/title>/ }
   its(:stderr) { should match /HTTP\/1.1 200 OK/ }
   its(:exit_status) { should eq 0 }
 end
+describe command("curl -v -x http://localhost:#{proxy_port} https://www.google.com"), :if => os[:family] == 'ubuntu' do
+  its(:stdout) { should match /<title>Google<\/title>/ }
+  its(:stderr) { should match /HTTP\/1.1 200 Connection established/ }
+  its(:stderr) { should match /CONNECT phase completed!/ }
+  its(:stderr) { should match /TLSv1.2 (IN)/ }
+  its(:stderr) { should match /TLSv1.2 (OUT)/ }
+  its(:exit_status) { should eq 0 }
+end
 
-describe command("curl -kvL -x http://localhost:#{proxy_port} https://www.cnn.com") do
+describe command("curl -kvL -x http://localhost:#{proxy_port} https://www.cnn.com"), :if => os[:family] == 'redhat' do
   its(:stdout) { should match /<!DOCTYPE html><html class="no-js"><head>/ }
   its(:stderr) { should match /HTTP\/1.1 200 OK/ }
+  its(:exit_status) { should eq 0 }
+end
+describe command("curl -kvL -x http://localhost:#{proxy_port} https://www.cnn.com"), :if => os[:family] == 'ubuntu' do
+  its(:stdout) { should match /<!DOCTYPE html><html class="no-js"><head>/ }
+  its(:stderr) { should match /HTTP\/1.1 200 Connection established/ }
+  its(:stderr) { should match /CONNECT phase completed!/ }
+  its(:stderr) { should match /TLSv1.2 (IN)/ }
+  its(:stderr) { should match /TLSv1.2 (OUT)/ }
   its(:exit_status) { should eq 0 }
 end
