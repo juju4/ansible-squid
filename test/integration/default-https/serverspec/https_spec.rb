c8b10fdd4b0d1495f52427a1f0c606af084528bd
juju4 <juju4@users.noreply.github.com>
2019-01-05 18:05:56 +0000

    squid openssl from deb source: update serverspec tests - missing escape

--- test/integration/default-https/serverspec/https_spec.rb
+++ test/integration/default-https/serverspec/https_spec.rb
@@ -14,8 +14,8 @@ describe command("curl -v -x http://localhost:#{proxy_port} https://www.google.c
   its(:stdout) { should match /<title>Google<\/title>/ }
   its(:stderr) { should match /HTTP\/1.1 200 Connection established/ }
   its(:stderr) { should match /CONNECT phase completed!/ }
-  its(:stderr) { should match /TLSv1.2 (IN)/ }
-  its(:stderr) { should match /TLSv1.2 (OUT)/ }
+  its(:stderr) { should match /TLSv1.2 \(IN\)/ }
+  its(:stderr) { should match /TLSv1.2 \(OUT\)/ }
   its(:exit_status) { should eq 0 }
 end
 
@@ -28,7 +28,7 @@ describe command("curl -kvL -x http://localhost:#{proxy_port} https://www.cnn.co
   its(:stdout) { should match /<!DOCTYPE html><html class="no-js"><head>/ }
   its(:stderr) { should match /HTTP\/1.1 200 Connection established/ }
   its(:stderr) { should match /CONNECT phase completed!/ }
-  its(:stderr) { should match /TLSv1.2 (IN)/ }
-  its(:stderr) { should match /TLSv1.2 (OUT)/ }
+  its(:stderr) { should match /TLSv1.2 \(IN\)/ }
+  its(:stderr) { should match /TLSv1.2 \(OUT\)/ }
   its(:exit_status) { should eq 0 }
 end
