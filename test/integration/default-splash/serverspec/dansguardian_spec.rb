26a6252e56bb2eacc19588b0ccda1986f0c7e2fb
juju4 <juju4@users.noreply.github.com>
2019-06-22 16:26:16 -0400

    test/default-splash: remove non-splash url testing

--- test/integration/default-splash/serverspec/dansguardian_spec.rb
+++ test/integration/default-splash/serverspec/dansguardian_spec.rb
@@ -21,24 +21,3 @@ end
 describe port(proxy_port), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   it { should be_listening }
 end
-
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.google.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stdout) { should match /<title>Google<\/title>/ }
-  its(:stderr) { should match /HTTP\/1.1 200 OK/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.cnn.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stderr) { should match /HTTP\/1.1 301 Moved Permanently/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.badboys.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stdout) { should match /<title>DansGuardian - Access Denied<\/title>/ }
-  its(:exit_status) { should eq 0 }
-end
-
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.eicar.org/download/eicar.com.txt"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stdout) { should match /<title>DansGuardian - Access Denied<\/title>/ }
-  its(:stdout) { should match /<b>Virus or bad content detected. Eicar-Test-Signature<\/b>/ }
-  its(:exit_status) { should eq 0 }
-end
-
--- test/integration/default-splash/serverspec/squid_spec.rb
+++ test/integration/default-splash/serverspec/squid_spec.rb
@@ -19,24 +19,3 @@ end
 describe port(3128) do
   it { should be_listening }
 end
-
-proxy_port = 3128
-
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.google.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stdout) { should match /<title>Google<\/title>/ }
-  its(:stderr) { should match /HTTP\/1.1 200 OK/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.cnn.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stderr) { should match /HTTP\/1.1 301 Moved Permanently/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.badboys.com"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stderr) { should match /301 Moved Permanently/ }
-  its(:exit_status) { should eq 0 }
-end
-
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.eicar.org/download/eicar.com.txt"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:stdout) { should include "EICAR-STANDARD-ANTIVIRUS-TEST-FILE!" }
-  its(:exit_status) { should eq 0 }
-end
