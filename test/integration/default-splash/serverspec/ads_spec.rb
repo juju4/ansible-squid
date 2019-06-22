4ea57166305c80c30fd2c6a62406fa69d3ff0b88
juju4 <juju4@users.noreply.github.com>
2019-06-22 17:37:43 -0400

    test/default-splash: remove non-splash url testing

--- test/integration/default-splash/serverspec/ads_spec.rb
+++ test/integration/default-splash/serverspec/ads_spec.rb
@@ -18,10 +18,3 @@ describe file('/etc/squid/squid.conf') do
   its(:content) { should match /acl ads dstdom_regex "\/etc\/squid\/ad_block.txt"/ }
 end
 
-describe command("curl -x http://localhost:#{proxy_port} http://www.adblockanalytics.com") do
-  its(:stdout) { should match /<p><b>Access Denied.<\/b><\/p>/ }
-#  its(:stdout) { should match /<title>DansGuardian - Access Denied<\/title>/ }
-#  its(:stdout) { should_not match /\(squid\/[0-9.]+\)<\/p>/ }
-  its(:exit_status) { should eq 0 }
-end
-
