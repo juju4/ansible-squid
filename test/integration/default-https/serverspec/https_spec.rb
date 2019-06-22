8359c073924b040f106a2b9f4c61603cafe780d0
juju4 <juju4@users.noreply.github.com>
2019-06-22 10:00:08 -0400

    test/default-https: google enforcing http/2 since 2 weeks?

--- test/integration/default-https/serverspec/https_spec.rb
+++ test/integration/default-https/serverspec/https_spec.rb
@@ -14,8 +14,12 @@ describe command("curl -v -x http://localhost:#{proxy_port} https://www.google.c
   its(:stdout) { should match /<title>Google<\/title>/ }
   its(:stderr) { should match /HTTP\/1.1 200 Connection established/ }
   its(:stderr) { should match /CONNECT phase completed!/ }
-  its(:stderr) { should match /TLSv1.2 \(IN\)/ }
-  its(:stderr) { should match /TLSv1.2 \(OUT\)/ }
+  its(:stderr) { should match /SSL connection using unknown \/ TLS_AES_256_GCM_SHA384/ }
+  its(:stderr) { should match /Using HTTP2, server supports multi-use/ }
+  its(:stderr) { should match /Connection state changed \(HTTP\/2 confirmed\)/ }
+  its(:stderr) { should match /GET \/ HTTP\/2/ }
+  its(:stderr) { should match /\(IN\), TLS Unknown, Unknown \(23\):/ }
+  its(:stderr) { should match /\(OUT\), TLS Unknown, Unknown \(23\):/ }
   its(:exit_status) { should eq 0 }
 end
 
