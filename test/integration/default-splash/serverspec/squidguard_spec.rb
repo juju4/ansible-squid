1be5f6c8416cb6f4083113aa0b688b3b58e33222
juju4 <juju4@users.noreply.github.com>
2019-06-22 19:07:06 -0400

    test/default-splash: remove non-splash url testing

--- test/integration/default-splash/serverspec/squidguard_spec.rb
+++ test/integration/default-splash/serverspec/squidguard_spec.rb
@@ -17,19 +17,3 @@ end
 describe file('/usr/sbin/squidGuard'), :if => os[:family] == 'redhat' && os[:release] == '6' do
   it { should be_executable }
 end
-
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.google.com"), :if => os[:family] == 'redhat' do
-  its(:stdout) { should match /<title>Google<\/title>/ }
-  its(:stderr) { should match /HTTP\/1.1 200 OK/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.cnn.com"), :if => os[:family] == 'redhat' do
-  its(:stderr) { should match /HTTP\/1.1 301 Moved Permanently/ }
-  its(:exit_status) { should eq 0 }
-end
-
-describe command("curl -x http://localhost:#{proxy_port} http://www.eicar.org/download/eicar.com.txt"), :if => os[:family] == 'redhat' && os[:release] == '6' do
-  its(:stdout) { should match /<b>Virus or bad content detected. Eicar-Test-Signature<\/b>/ }
-  its(:exit_status) { should eq 0 }
-end
-
