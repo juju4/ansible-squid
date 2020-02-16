36dba7f3cc5675f61f54f0402e0678100cd8beba
juju4 <juju4@users.noreply.github.com>
2020-02-16 07:34:44 -0500

    clamav: Eicar signature name change

--- test/integration/default-clamd/serverspec/dansguardian_spec.rb
+++ test/integration/default-clamd/serverspec/dansguardian_spec.rb
@@ -38,7 +38,7 @@ end
 
 describe command("curl -v -x http://localhost:#{proxy_port} http://www.eicar.org/download/eicar.com.txt"), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   its(:stdout) { should match /<title>DansGuardian - Access Denied<\/title>/ }
-  its(:stdout) { should match /<b>Virus or bad content detected. Eicar-Test-Signature<\/b>/ }
+  its(:stdout) { should match /<b>Virus or bad content detected. Win.Test.EICAR_HDB-1<\/b>/ }
   its(:exit_status) { should eq 0 }
 end
 
@@ -49,8 +49,8 @@ end
 describe file('/var/log/dansguardian/access.log'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   its(:content) { should match /http:\/\/www.google.com \*SCANNED\*/ }
   its(:content) { should match /http:\/\/www.badboys.com \*DENIED\* Banned site:/ }
-  its(:content) { should match /http:\/\/www.eicar.org\/download\/eicar.com.txt \*INFECTED\* \*DENIED\* Virus or bad content detected. Eicar-Test-Signature/ }
+  its(:content) { should match /http:\/\/www.eicar.org\/download\/eicar.com.txt \*INFECTED\* \*DENIED\* Virus or bad content detected. Win.Test.EICAR_HDB-1/ }
 end
 describe file('/var/log/clamav/clamav.log'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
-  its(:content) { should match /Eicar-Test-Signature/ }
+  its(:content) { should match /Win.Test.EICAR_HDB-1/ }
 end
