f843b5eb62b51d9e67f88686c08afa8cac6b8f60
juju4 <juju4@users.noreply.github.com>
2020-03-07 18:50:15 -0500

    test/default: regroup dansguardian/e2guardian tests in one file, ensure sites are banned for 19.10

--- test/integration/default/serverspec/ads_spec.rb
+++ test/integration/default/serverspec/ads_spec.rb
@@ -3,10 +3,15 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-if os[:family] == 'ubuntu' || os[:family] == 'debian'
+if (os[:family] == 'ubuntu' && os[:release] == '19.10')
+  set denied_banner = 'E2Guardian - Access Denied'
+  proxy_port = 8080
+else
+  set denied_banner = 'DansGuardian - Access Denied'
   proxy_port = 8080
 end
 if os[:family] == 'redhat'
+  set denied_banner = 'Access Denied'
   proxy_port = 3128
 end
 
@@ -19,8 +24,6 @@ describe file('/etc/squid/squid.conf') do
 end
 
 describe command("curl -x http://localhost:#{proxy_port} http://www.adblockanalytics.com") do
-  its(:stdout) { should match /<p><b>Access Denied.<\/b><\/p>/ }
-#  its(:stdout) { should match /<title>DansGuardian - Access Denied<\/title>/ }
-#  its(:stdout) { should_not match /\(squid\/[0-9.]+\)<\/p>/ }
+  its(:stdout) { should match /#{denied_banner}/ }
   its(:exit_status) { should eq 0 }
 end
