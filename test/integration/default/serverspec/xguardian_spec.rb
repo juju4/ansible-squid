cc6db3a90d9427f71470275ea2bf258f84c11f80
juju4 <juju4@users.noreply.github.com>
2020-03-07 19:40:03 -0500

    test/default: no guardian on centos, fix some rules

--- test/integration/default/serverspec/xguardian_spec.rb
+++ test/integration/default/serverspec/xguardian_spec.rb
@@ -8,41 +8,43 @@ if (os[:family] == 'ubuntu' && os[:release] == '19.10')
   set denied_banner = 'E2Guardian - Access Denied'
 else
   set guardian_pkg = 'dansguardian'
-  set denied_banner = 'DansGuardian - Access Denied'
+  set denied_banner = '<p><b>Access Denied.</b></p>'
 end
 proxy_port = 8080
 
-describe package(guardian_pkg) do
-  it { should be_installed }
-end
+if (os[:family] == 'ubuntu' || os[:family] == 'debian')
+  describe package(guardian_pkg) do
+    it { should be_installed }
+  end
 
-describe service(guardian_pkg) do
-  it { should be_enabled }
-  it { should be_running }
-end  
+  describe service(guardian_pkg) do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
-describe file("/usr/sbin/#{guardian_pkg}") do
-  it { should be_executable }
-end
+  describe file("/usr/sbin/#{guardian_pkg}") do
+    it { should be_executable }
+  end
 
-describe port(proxy_port) do
-  it { should be_listening }
-end
+  describe port(proxy_port) do
+    it { should be_listening }
+  end
 
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.google.com") do
-  its(:stdout) { should match /<title>Google<\/title>/ }
-  its(:stderr) { should match /HTTP\/1.1 200 OK/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.cnn.com") do
-  its(:stderr) { should match /HTTP\/1.1 301 Moved Permanently/ }
-  its(:exit_status) { should eq 0 }
-end
-describe command("curl -v -x http://localhost:#{proxy_port} http://www.badboys.com") do
-  its(:stdout) { should match /<title>#{denied_banner}<\/title>/ }
-  its(:exit_status) { should eq 0 }
-end
+  describe command("curl -v -x http://localhost:#{proxy_port} http://www.google.com") do
+    its(:stdout) { should match /<title>Google<\/title>/ }
+    its(:stderr) { should match /HTTP\/1.1 200 OK/ }
+    its(:exit_status) { should eq 0 }
+  end
+  describe command("curl -v -x http://localhost:#{proxy_port} http://www.cnn.com") do
+    its(:stderr) { should match /HTTP\/1.1 301 Moved Permanently/ }
+    its(:exit_status) { should eq 0 }
+  end
+  describe command("curl -v -x http://localhost:#{proxy_port} http://www.badboys.com") do
+    its(:stdout) { should match /#{denied_banner}/ }
+    its(:exit_status) { should eq 0 }
+  end
 
-describe file("/var/log/#{guardian_pkg}/access.log") do
-  its(:content) { should match /http:\/\/www.badboys.com .* \*DENIED\*/ }
+  describe file("/var/log/#{guardian_pkg}/access.log") do
+    its(:content) { should match /http:\/\/www.badboys.com *\*DENIED\*/ }
+  end
 end
